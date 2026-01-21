e08103a3e994ce7d8d35344ed2ba942f8fb47f97
Nelson Enrique Sierra Perez <nelsonenriquesierraperez@gmail.com>
2026-01-21 10:01:28 -0500

    Serve media uploads in development

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
 from apps.usuarios.serializers import CustomTokenObtainPairView
 from .api_router import router
@@ -34,3 +36,6 @@ urlpatterns = [
     # Autenticación de DRF (para browsable API)
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
