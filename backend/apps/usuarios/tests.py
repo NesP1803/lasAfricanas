17b7ff54ccea3e54768b51a79562f200f22a5473
Nelson Enrique Sierra Perez <nelsonenriquesierraperez@gmail.com>
2026-02-05 19:06:44 -0500

    Fix usuario creation when modulos_permitidos is null

--- backend/apps/usuarios/tests.py
+++ backend/apps/usuarios/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.usuarios.serializers import UsuarioSerializer
+
+
+class UsuarioSerializerTests(TestCase):
+    def test_create_asigna_modulos_vacio_cuando_no_se_envia(self):
+        serializer = UsuarioSerializer(
+            data={
+                'username': 'sin_modulos',
+                'password': 'pass1234',
+                'tipo_usuario': 'ADMIN',
+            }
+        )
+
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        usuario = serializer.save()
+
+        self.assertEqual(usuario.modulos_permitidos, {})
+
+    def test_create_asigna_modulos_vacio_cuando_llega_null(self):
+        serializer = UsuarioSerializer(
+            data={
+                'username': 'modulos_null',
+                'password': 'pass1234',
+                'tipo_usuario': 'VENDEDOR',
+                'modulos_permitidos': None,
+            }
+        )
+
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        usuario = serializer.save()
+
+        self.assertEqual(usuario.modulos_permitidos, {})
