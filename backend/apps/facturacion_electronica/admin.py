5afd2e5e109bd4e6e2634e305b0972b9f54c05d7
Nelson Enrique Sierra Perez <nelsonenriquesierraperez@gmail.com>
2026-03-29 18:42:08 -0500

    Refactoriza facturacion_electronica a capa de integración

--- backend/apps/facturacion_electronica/admin.py
+++ backend/apps/facturacion_electronica/admin.py
@@ -1,27 +1,14 @@
 from django.contrib import admin
 
-from apps.facturacion.models import FacturaElectronica
 from .models import (
-    DocumentoSoporteElectronico,
     FactusToken,
     HomologacionMedioPago,
     HomologacionMunicipio,
     HomologacionTributo,
     HomologacionUnidadMedida,
-    NotaCreditoElectronica,
 )
 
-
-@admin.register(FacturaElectronica)
-class FacturaElectronicaAdmin(admin.ModelAdmin):
-    list_display = ('number', 'venta', 'status', 'created_at')
-    list_filter = ('status',)
-    search_fields = ('number', 'venta__numero_comprobante', 'uuid', 'cufe')
-    readonly_fields = ('response_json',)
-
-
-admin.site.register(NotaCreditoElectronica)
-admin.site.register(DocumentoSoporteElectronico)
+# Esta app registra solo administración de integración Factus, no dominio de negocio.
 admin.site.register(FactusToken)
 admin.site.register(HomologacionMunicipio)
 admin.site.register(HomologacionTributo)
